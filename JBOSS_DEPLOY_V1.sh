f4978ee3d68d233c528e1572ccf8bd35823d902c
srinu2991 <53953625+srinu2991@users.noreply.github.com>
2024-05-21 19:54:44 -0500

    Update JBOSS_DEPLOY_V1.sh

--- JBOSS_DEPLOY_V1.sh
+++ JBOSS_DEPLOY_V1.sh
@@ -153,8 +153,8 @@ function clean_up_folders() {
 slave_dirs=(
     "$ENV_HOME/$environment_name/domain/servers/*/data/content/*"
     "$ENV_HOME/$environment_name/domain/servers/*/tmp/*"
-    "ENV_HOME/$environment_name/domain/servers/*/data/infinispan/*"
-    "ENV_HOME/$environment_name/domain/servers/*/data/wsdl/*"
+    "$ENV_HOME/$environment_name/domain/servers/*/data/infinispan/*"
+    "$ENV_HOME/$environment_name/domain/servers/*/data/wsdl/*"
 )
 for dir in "\${slave_dirs[@]}"; do
     if compgen -G "\$dir" > /dev/null; then
